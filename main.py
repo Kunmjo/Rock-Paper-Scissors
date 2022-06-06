c221f85d19ea1d836ce3488e9cc76e57e066f8f5
Kunmjo <81991490+Kunmjo@users.noreply.github.com>
2022-06-06 13:46:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import random
 
 while True:
-    user_action = input("Enter a choice (R, P, S): ")
+    user_action = input("Enter a choice ('R' = Rock, 'P' = Paper, 'S' = Scissors): ").capitalize()
     possible_actions = ["R", "P", "S"]
     computer_action = random.choice(possible_actions)
+    while user_action not in possible_actions:
+        print("Not amongst our options")
+        user_action = input("Enter a choice ('R' = Rock, 'P' = Paper, 'S' = Scissors): ").capitalize()
+ 
     print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
-
+    
+    
     if user_action == computer_action:
         print(f"Both players selected {user_action}. It's a tie!")
     elif user_action == "R":
